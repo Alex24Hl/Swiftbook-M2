b626c92685a67525c70e0b03c5ccf602d5ab2af5
Aleksander Holod <xolod2002@yandex.ru>
2022-01-18 12:19:35 +0300

    Сделан проект "Slider"

---
+++ Homework-2.1.Slider/Homework-2.1.Slider/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Homework-2.1.Slider
+//
+//  Created by Александр Холод on 17.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+

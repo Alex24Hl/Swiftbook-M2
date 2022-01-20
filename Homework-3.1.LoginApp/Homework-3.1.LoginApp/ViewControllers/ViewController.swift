61f977beb803535e2679f7c4c15b7ac7430abedd
Aleksander Holod <xolod2002@yandex.ru>
2022-01-20 16:39:29 +0300

    initial commit

---
+++ Homework-3.1.LoginApp/Homework-3.1.LoginApp/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-3.1.LoginApp
+//
+//  Created by Александр Холод on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

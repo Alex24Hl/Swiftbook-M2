71d643eb1819e316f2e0f545d3f00133eb40f0a4
Aleksander Holod <xolod2002@yandex.ru>
2022-02-08 17:49:26 +0300

    initial commit

---
+++ Homework-5.1.PersonalQuiz/Homework-5.1.PersonalQuiz/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-5.1.PersonalQuiz
+//
+//  Created by Александр Холод on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
